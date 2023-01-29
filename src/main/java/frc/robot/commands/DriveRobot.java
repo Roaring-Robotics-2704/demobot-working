c4c8931111a22997b9e54f7a1983c0bf766efb83
MMarchjr <103056947+mmarchjr@users.noreply.github.com>
2023-01-29 19:33:11 +0000

    fix bugs

--- src/main/java/frc/robot/commands/DriveRobot.java
+++ src/main/java/frc/robot/commands/DriveRobot.java
@@ -3,17 +3,17 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-
-
 public class DriveRobot extends CommandBase {
   /** Creates a new DriveRobot. */
   public DriveRobot() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(RobotContainer.m_Drivetrain);
   }
+
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
@@ -21,43 +21,43 @@ public class DriveRobot extends CommandBase {
     RobotContainer.m_imu.calibrate();
     RobotContainer.m_imu.reset();
   }
-public boolean mode;
-private double angle;
-public boolean turbo;
-public double turboamount;
+
+  public boolean mode;
+  private double angle;
+  public boolean turbo;
+  public double turboamount;
   // Called every time the scheduler runs while the command is scheduled.
-  
+
   @Override
   public void execute() {
     turbo = RobotContainer.xbox.getRightBumper();
     if (turbo) {
       turboamount = 1;
-    }
-    else {
+    } else {
       turboamount = Constants.c_speedcap;
     }
-    double joystickz = RobotContainer.xbox.getLeftX();   //getRawAxis(Constants.c_leftJoystickAxisx);
-    double joystickx = RobotContainer.xbox.getRightX();  //getRawAxis(Constants.c_rightJoystickAxisx);
-    double joysticky = -RobotContainer.xbox.getRightY(); //getRawAxis(Constants.c_rightJoystickAxisy);
-    double outputx = joystickx*turboamount;
-    double outputy = joysticky*turboamount;
-    double outputz = joystickz*turboamount;
+    double joystickz = RobotContainer.xbox.getLeftX(); // getRawAxis(Constants.c_leftJoystickAxisx);
+    double joystickx = RobotContainer.xbox.getRightX(); // getRawAxis(Constants.c_rightJoystickAxisx);
+    double joysticky = -RobotContainer.xbox.getRightY(); // getRawAxis(Constants.c_rightJoystickAxisy);
+    double outputx = joystickx * turboamount;
+    double outputy = joysticky * turboamount;
+    double outputz = joystickz * turboamount;
     mode = RobotContainer.DriveMode.getSelected();
     if (mode) {
       angle = -RobotContainer.m_imu.getAngle();
-      if (RobotContainer.xbox.getLeftBumper()){
+      if (RobotContainer.xbox.getLeftBumper()) {
         RobotContainer.m_imu.reset();
       }
+    } else {
+      angle = 0;
     }
-    else {
-      angle = 0;  
-    }
-    RobotContainer.m_Drivetrain.driveCartesian(outputy,outputx,outputz,angle);
+    RobotContainer.m_Drivetrain.driveCartesian(outputy, outputx, outputz, angle);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
